d334b920ae7314892d9e0f266a8a6c71a3aeed17
DayongLu <ludayong20@gmail.com>
2017-10-02 22:26:37 -0400

    add register service and configure with config server as well as eureka server

--- src/main/java/com/dlu/registerservice/config/RegisterConfig.java
+++ src/main/java/com/dlu/registerservice/config/RegisterConfig.java
@@ -3,9 +3,11 @@ package com.dlu.registerservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 
+@RefreshScope
 @Configuration
 @ConfigurationProperties(prefix = "application")
 public class RegisterConfig {
